30ca6be1eb1c209d3ded979f8a5956edc65f7884
Feiks <37041998+Feiks@users.noreply.github.com>
2022-06-07 15:57:51 +0600

    Add files via upload

---
+++ src/main/java/net/javaguides/springboot/model/Receiver.java
@@ -0,0 +1,51 @@
+package net.javaguides.springboot.model;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+public class Receiver {
+
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String code;
+
+    public Receiver() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
+
+
+
